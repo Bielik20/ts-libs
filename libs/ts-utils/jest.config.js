195a2989f1feaa4840941b2bc27c41d934a88c4b
Damian Bielecki <bielik20@gmail.com>
2021-05-03 13:38:17 +0200

    chore: generate ts-utils

---
+++ libs/ts-utils/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  displayName: 'ts-utils',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/libs/ts-utils',
+};
