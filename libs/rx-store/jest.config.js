e0a8f9f55ddbe0ec9d29265e4d2522456aa9ad47
Damian Bielecki <bielik20@gmail.com>
2021-05-08 10:27:10 +0200

    chore: generate rx-store

---
+++ libs/rx-store/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  displayName: 'rx-store',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/libs/rx-store',
+};
