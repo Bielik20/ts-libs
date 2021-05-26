bdc00426fb70ada110efdda419f89b73e2426bbd
Damian Bielecki <bielik20@gmail.com>
2021-05-26 15:16:06 +0200

    chore: improve perf of ProductsFlagsCont

--- apps/react-demo/src/products/ui/products-flags.cont.tsx
+++ apps/react-demo/src/products/ui/products-flags.cont.tsx
@@ -1,6 +1,7 @@
 import { useDependency } from '@ns3/react-di';
 import { useStreamValue } from '@ns3/ts-utils';
 import { FunctionComponent } from 'react';
+import { combineLatest } from 'rxjs';
 import { ProductsConnectingSet } from '../services/products-connecting.set';
 import { ProductsDeletingSet } from '../services/products-deleting.set';
 import { ProductsUpdatingSet } from '../services/products-updating.set';
@@ -10,13 +11,14 @@ export const ProductsFlagsCont: FunctionComponent = () => {
   const connecting = useDependency(ProductsConnectingSet);
   const deleting = useDependency(ProductsDeletingSet);
   const updating = useDependency(ProductsUpdatingSet);
-  const connectingKeys = useStreamValue(() => connecting.keys(), []);
-  const deletingKeys = useStreamValue(() => deleting.keys(), []);
-  const updatingKeys = useStreamValue(() => updating.keys(), []);
+  const [connectingKeys, deletingKeys, updatingKeys] = useStreamValue(
+    () => combineLatest([connecting.keys(), deleting.keys(), updating.keys()]),
+    [],
+  );
 
   return (
     <>
-      <ProductsFlagsCard title="Connecting" flags={connectingKeys || []} />
+      <ProductsFlagsCard title="Connecting" flags={connectingKeys} />
       <ProductsFlagsCard title="Deleting" flags={deletingKeys} />
       <ProductsFlagsCard title="Updating" flags={updatingKeys} />
     </>
