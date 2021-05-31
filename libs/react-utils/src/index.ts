a8841b9de81acc70c2c23ccd7f623bf866f5c4f4
Damian Bielecki <bielik20@gmail.com>
2021-05-31 20:49:17 +0200

    chore: remove useCombineLatestValue

--- libs/react-utils/src/index.ts
+++ libs/react-utils/src/index.ts
@@ -1,4 +1,3 @@
-export * from './lib/hooks/use-combine-latest-value';
 export * from './lib/hooks/use-prevent-unload';
 export * from './lib/hooks/use-promise';
 export * from './lib/hooks/use-stream';
