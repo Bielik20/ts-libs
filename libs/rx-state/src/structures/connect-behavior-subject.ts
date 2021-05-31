3e9061560c586e262666ccf89b3800fd43453100
Damian Bielecki <bielik20@gmail.com>
2021-05-31 16:45:58 +0200

    feat(rx-state): add ConnectBehaviorSubject

---
+++ libs/rx-state/src/structures/connect-behavior-subject.ts
@@ -0,0 +1,34 @@
+import { ConnectionManager, ConnectionManagerConfig } from '@ns3/rx-state';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+interface ConnectBehaviorSubjectOptions extends ConnectionManagerConfig {
+  connecting$?: BehaviorSubject<boolean>;
+}
+
+export class ConnectBehaviorSubject<T> extends BehaviorSubject<T | undefined> {
+  protected readonly connectionManager: ConnectionManager<T>;
+
+  constructor({ connecting$, ...options }: ConnectBehaviorSubjectOptions) {
+    super(undefined);
+    this.connectionManager = new ConnectionManager<T>(options, {
+      connecting: connecting$ && (() => connecting$.value !== true && connecting$.next(true)),
+      connected: connecting$ && (() => connecting$.value !== false && connecting$.next(false)),
+      has: () => this.value !== undefined,
+      get$: () => this.asObservable(),
+      set: (value) => this.next(value),
+    });
+  }
+
+  next(value: T | undefined) {
+    this.connectionManager.validate();
+    super.next(value);
+  }
+
+  connect$(factory: () => Observable<T>): Observable<T> {
+    return this.connectionManager.connect$(factory);
+  }
+
+  invalidate(): void {
+    return this.connectionManager.invalidate();
+  }
+}
