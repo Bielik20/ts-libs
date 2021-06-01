287d4c0a549d7e1cdf2f44ae092f4b3bc5f4ee3c
Damian Bielecki <bielik20@gmail.com>
2021-06-01 18:06:44 +0200

    chore: use useRef for keeping subscription

--- libs/react-utils/src/lib/hooks/use-stream-value.ts
+++ libs/react-utils/src/lib/hooks/use-stream-value.ts
@@ -1,5 +1,5 @@
 import { FactoryOrValue, Falsy, unpackFactoryOrValue } from '@ns3/ts-utils';
-import { DependencyList, useMemo, useState } from 'react';
+import { DependencyList, useMemo, useRef } from 'react';
 import { BehaviorSubject, Observable, Subscription } from 'rxjs';
 import { useBehaviorSubjectValue } from './use-behavior-subject-value';
 
@@ -7,24 +7,22 @@ export function useStreamValue<T>(
   factory: FactoryOrValue<Falsy | Observable<T>>,
   deps?: DependencyList,
 ): T | undefined {
-  const [subscription, setSubscription] = useState<Subscription | undefined>(undefined);
+  const sub = useRef<Subscription>({ unsubscribe: () => null } as Subscription);
   const behaviorSubject$ = useMemo(() => new BehaviorSubject<T | undefined>(undefined), []);
 
   useMemo(() => {
     const stream$ = unpackFactoryOrValue(factory);
 
-    subscription && subscription.unsubscribe();
+    sub.current.unsubscribe();
 
     if (!stream$) {
       return behaviorSubject$.next(undefined);
     }
 
-    setSubscription(
-      stream$.subscribe(
-        (v) => behaviorSubject$.next(v),
-        () => behaviorSubject$.next(undefined),
-      ),
-    );
+    sub.current = stream$.subscribe({
+      next: (v) => behaviorSubject$.next(v),
+      error: () => behaviorSubject$.next(undefined),
+    });
   }, deps);
 
   return useBehaviorSubjectValue(behaviorSubject$);
