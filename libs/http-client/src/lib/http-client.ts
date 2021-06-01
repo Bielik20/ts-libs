45b33e35e0fed334d73362b667a4a67b0f808872
Damian Bielecki <bielik20@gmail.com>
2021-06-01 09:18:48 +0200

    chore: update RxJs and http-client

--- libs/http-client/src/lib/http-client.ts
+++ libs/http-client/src/lib/http-client.ts
@@ -1,52 +1,63 @@
 import { Injectable } from '@wikia/dependency-injection';
 import { Observable, of } from 'rxjs';
-import { ajax } from 'rxjs/ajax';
+import { ajax, AjaxConfig, AjaxResponse } from 'rxjs/ajax';
 import { concatMap } from 'rxjs/operators';
 import { HttpInterceptors } from './http-interceptors';
 import { HttpHandler } from './models/http-handler';
-import { HttpRequest } from './models/http-request';
-import { HttpResponse } from './models/http-response';
 
-type Response<R> = Observable<HttpResponse<R>>;
-
-const BackendHandler: HttpHandler = (req) => {
-  return ajax(req);
+const BackendHandler = <T>(req: AjaxConfig) => {
+  return ajax<T>(req);
 };
 
 @Injectable()
 export class HttpClient {
-  private _handler: HttpHandler;
+  private _handler: HttpHandler<any>;
 
   constructor(private interceptors: HttpInterceptors) {}
 
-  get<T = unknown>(url: string, headers?: Record<string, any>): Response<T> {
-    return this.makeRequest({ method: 'GET', url, headers });
+  get<T = unknown>(url: string, headers?: Record<string, any>): Observable<AjaxResponse<T>> {
+    return this.makeRequest<T>({ method: 'GET', url, headers });
   }
 
-  patch<T = unknown>(url: string, body?: unknown, headers?: Record<string, any>): Response<T> {
-    return this.makeRequest({ method: 'PATCH', url, headers, body });
+  patch<T = unknown>(
+    url: string,
+    body?: unknown,
+    headers?: Readonly<Record<string, any>>,
+  ): Observable<AjaxResponse<T>> {
+    return this.makeRequest<T>({ method: 'PATCH', url, headers, body });
   }
 
-  post<T = unknown>(url: string, body?: unknown, headers?: Record<string, any>): Response<T> {
-    return this.makeRequest({ method: 'POST', url, headers, body });
+  post<T = unknown>(
+    url: string,
+    body?: unknown,
+    headers?: Readonly<Record<string, any>>,
+  ): Observable<AjaxResponse<T>> {
+    return this.makeRequest<T>({ method: 'POST', url, headers, body });
   }
 
-  put<T = unknown>(url: string, body?: unknown, headers?: Record<string, any>): Response<T> {
-    return this.makeRequest({ method: 'PUT', url, headers, body });
+  put<T = unknown>(
+    url: string,
+    body?: unknown,
+    headers?: Readonly<Record<string, any>>,
+  ): Observable<AjaxResponse<T>> {
+    return this.makeRequest<T>({ method: 'PUT', url, headers, body });
   }
 
-  delete<T = unknown>(url: string, headers?: Record<string, any>): Response<T> {
-    return this.makeRequest({ method: 'DELETE', url, headers });
+  delete<T = unknown>(
+    url: string,
+    headers?: Readonly<Record<string, any>>,
+  ): Observable<AjaxResponse<T>> {
+    return this.makeRequest<T>({ method: 'DELETE', url, headers });
   }
 
-  private makeRequest(req: HttpRequest): Observable<HttpResponse> {
+  private makeRequest<T>(req: AjaxConfig): Observable<AjaxResponse<T>> {
     const request = this.createRequest(req);
-    const handler = this.getHandler();
+    const handler = this.getHandler<T>();
 
     return of(request).pipe(concatMap((req) => handler(req)));
   }
 
-  private createRequest(req: HttpRequest): HttpRequest {
+  private createRequest(req: AjaxConfig): AjaxConfig {
     return {
       ...req,
       headers: {
@@ -56,19 +67,19 @@ export class HttpClient {
     };
   }
 
-  private getHandler(): HttpHandler {
+  private getHandler<T>(): HttpHandler<T> {
     if (!this._handler) {
-      this._handler = this.createHandler();
+      this._handler = this.createHandler<T>();
     }
 
     return this._handler;
   }
 
-  private createHandler(): HttpHandler {
+  private createHandler<T>(): HttpHandler<T> {
     const interceptors = this.interceptors.get();
 
-    return interceptors.reduceRight<HttpHandler>(
-      (next, interceptor) => (req: HttpRequest) => interceptor.intercept(req, next),
+    return interceptors.reduceRight<HttpHandler<T>>(
+      (next, interceptor) => (req) => interceptor.intercept<T>(req, next),
       BackendHandler,
     );
   }
--- libs/http-client/src/lib/models/http-request.ts
+++
@@ -1,8 +0,0 @@
-type Method = 'DELETE' | 'GET' | 'PUT' | 'PATCH' | 'POST';
-
-export type HttpRequest<T = unknown> = {
-  url: string;
-  method: Method;
-  headers?: Record<string, any>;
-  body?: T;
-};
