bb740284d3ab43bf262a1a25554eb41fddf3640b
Damian Bielecki <bielik20@gmail.com>
2021-06-02 18:12:03 +0200

    fix: remove tap-once deprecation

--- libs/ts-utils/src/lib/rxjs/tap-once.ts
+++ libs/ts-utils/src/lib/rxjs/tap-once.ts
@@ -1,11 +1,15 @@
-import { merge, Observable } from 'rxjs';
+import { merge, MonoTypeOperatorFunction, Observable, Observer } from 'rxjs';
 import { filter, take, tap } from 'rxjs/operators';
 
-export function tapOnce<T>(next?: (x: T) => void, error?: (e: any) => void, complete?: () => void) {
+export function tapOnce<T>(next: (value: T) => void): MonoTypeOperatorFunction<T>;
+export function tapOnce<T>(observer?: Partial<Observer<T>>): MonoTypeOperatorFunction<T>;
+export function tapOnce<T>(
+  observerOrNext?: Partial<Observer<T>> | ((value: T) => void) | null,
+): MonoTypeOperatorFunction<T> {
   return (source: Observable<T>) => {
     const tapSource = source.pipe(
       take(1),
-      tap(next, error, complete),
+      tap(observerOrNext as any),
       filter(() => false),
     );
 
