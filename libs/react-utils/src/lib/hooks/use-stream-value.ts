a735ab4380ff359dc2ebb48f7546f0dd814fa544
Damian Bielecki <bielik20@gmail.com>
2021-06-02 08:57:15 +0200

    refactor: use useStreamInternal to unpack stream

--- libs/react-utils/src/lib/hooks/use-stream-value.ts
+++ libs/react-utils/src/lib/hooks/use-stream-value.ts
@@ -1,33 +1,23 @@
-import { FactoryOrValue, Falsy, unpackFactoryOrValue } from '@ns3/ts-utils';
-import { DependencyList, useEffect, useMemo, useRef } from 'react';
-import { BehaviorSubject, Observable, Subscription } from 'rxjs';
-import { useBehaviorSubjectValue } from './use-behavior-subject-value';
+import { FactoryOrValue, Falsy } from '@ns3/ts-utils';
+import { DependencyList, useDebugValue } from 'react';
+import { Observable } from 'rxjs';
+import { useStreamInternal } from './use-stream-internal';
 
 export function useStreamValue<T>(
   factory: FactoryOrValue<Falsy | Observable<T>>,
   deps?: DependencyList,
 ): T | undefined {
-  const sub = useRef<Pick<Subscription, 'unsubscribe'>>({ unsubscribe: () => null });
+  const value = useStreamInternal(
+    {
+      initial: undefined,
+      next: (v) => v,
+      error: () => undefined,
+    },
+    factory,
+    deps,
+  );
 
-  const behaviorSubject$ = useMemo(() => {
-    const behaviorSubject$ = new BehaviorSubject<T | undefined>(undefined);
-    const stream$ = unpackFactoryOrValue(factory);
+  useDebugValue(value);
 
-    sub.current.unsubscribe();
-
-    if (stream$) {
-      sub.current = stream$.subscribe({
-        next: (v) => behaviorSubject$.next(v),
-        error: () => behaviorSubject$.next(undefined),
-      });
-    }
-
-    return behaviorSubject$;
-  }, deps);
-
-  useEffect(() => {
-    return () => sub.current.unsubscribe();
-  }, []);
-
-  return useBehaviorSubjectValue(behaviorSubject$);
+  return value;
 }
