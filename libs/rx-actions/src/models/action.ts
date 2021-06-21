3ac416001b36b62fdcb04c2d07ef9a3e5d3cde62
Damian Bielecki <bielik20@gmail.com>
2021-06-21 19:46:56 +0200

    chore(rx-actions): add action creator

---
+++ libs/rx-actions/src/models/action.ts
@@ -0,0 +1,85 @@
+import { defer, Observable, ObservableInput, ObservedValueOf, of, Subject } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+const ACTION_METADATA_KEY = '__@ns3/rx-actions_action__';
+
+export type Action<I, O> = ActionExecutor<I, O> & {
+  invoked$: Observable<ActionInvoked<I>>;
+  succeeded$: Observable<ActionSucceeded<I, O>>;
+  failed$: Observable<ActionFailed<I>>;
+  completed$: Observable<ActionCompleted<I>>;
+  /**
+   * @internal
+   */
+  [ACTION_METADATA_KEY]: true;
+};
+
+type ActionExecutor<I, O> = I extends undefined ? () => Observable<O> : (input: I) => Observable<O>;
+
+export interface ActionInvoked<I> {
+  id: number;
+  input: I;
+}
+
+export interface ActionSucceeded<I, O> {
+  id: number;
+  input: I;
+  output: O;
+}
+
+export interface ActionFailed<I> {
+  id: number;
+  input: I;
+  error: any;
+}
+
+export interface ActionCompleted<I> {
+  id: number;
+  input: I;
+}
+
+export function action(): Action<undefined, undefined>;
+export function action(factory: () => void): Action<undefined, undefined>;
+export function action<O extends ObservableInput<any>>(
+  factory: () => O,
+): Action<undefined, ObservedValueOf<O>>;
+export function action<I>(factory: (input: I) => void): Action<I, undefined>;
+export function action<I, O extends ObservableInput<any>>(
+  factory: (input: I) => O = () => undefined as any,
+): Action<I, ObservedValueOf<O>> {
+  const invoked$$ = new Subject<ActionInvoked<I>>();
+  const succeeded$$ = new Subject<ActionSucceeded<I, ObservedValueOf<O>>>();
+  const failed$$ = new Subject<ActionFailed<I>>();
+  const completed$$ = new Subject<ActionCompleted<I>>();
+  let globalId = 0;
+
+  const result = (input: I) => {
+    const id = ++globalId;
+
+    return defer(() => {
+      invoked$$.next({ id, input });
+
+      return factory(input) ?? of(undefined);
+    }).pipe(
+      tap({
+        next: (output) => succeeded$$.next({ id, input, output }),
+        error: (error) => failed$$.next({ id, input, error }),
+        complete: () => completed$$.next({ id, input }),
+      }),
+    );
+  };
+
+  Object.defineProperties(result, {
+    invoked$: { value: invoked$$.asObservable() },
+    succeeded$: { value: succeeded$$.asObservable() },
+    failed$: { value: failed$$.asObservable() },
+    completed$: { value: completed$$.asObservable() },
+    [ACTION_METADATA_KEY]: { value: true },
+  });
+
+  return result as Action<I, ObservedValueOf<O>>;
+}
+
+export function isAction(input: any): input is Action<unknown, unknown> {
+  return typeof input === 'function' && ACTION_METADATA_KEY in input;
+}
