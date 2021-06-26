a766dd21bb62460478f465ca56951680b918c898
Damian Bielecki <bielik20@gmail.com>
2021-06-26 10:11:33 +0200

    chore: add separate generators for web-lib and react-lib

--- tools/generators/web-lib/index.ts
+++ tools/generators/web-lib/index.ts
@@ -6,23 +6,23 @@ import {
   updateProjectConfiguration,
 } from '@nrwl/devkit';
 import { Linter } from '@nrwl/linter';
-import { libraryGenerator } from '@nrwl/react';
+import { libraryGenerator } from '@nrwl/workspace';
 import npmGenerator from '@ns3/nx-npm/src/generators/npm/generator';
 import { Schema } from './schema';
 
 export default async function (host: Tree, schema: Schema) {
   await libraryGenerator(host, {
     name: schema.name,
-    style: schema.style,
     tags: schema.tags,
     skipTsConfig: false,
     skipFormat: false,
     unitTestRunner: 'jest',
     linter: Linter.EsLint,
-    publishable: true,
-    importPath: `@ns3/${schema.name}`,
+    strict: true,
+    buildable: true,
+    babelJest: true,
   });
-  await updateRollupConfig(host, { project: schema.name });
+  await updateWorkspaceConfig(host, { project: schema.name });
   await npmGenerator(host, { project: schema.name, skipFormat: true, access: 'public' });
   await formatFiles(host);
 
@@ -31,11 +31,28 @@ export default async function (host: Tree, schema: Schema) {
   };
 }
 
-function updateRollupConfig(tree: Tree, schema: { project: string }) {
+function updateWorkspaceConfig(tree: Tree, schema: { project: string }) {
   const projectConfig = readProjectConfiguration(tree, schema.project);
 
-  projectConfig.targets.build.options.rollupConfig = './tools/src/plugins/react-rollup-plugin';
-  projectConfig.targets.build.options.external = [...projectConfig.targets.build.options.external, "tslib"];
+  projectConfig.targets.build = {
+    executor: '@nrwl/web:package',
+    outputs: ['{options.outputPath}'],
+    options: {
+      outputPath: `dist/${projectConfig.root}`,
+      tsConfig: `${projectConfig.root}/tsconfig.lib.json`,
+      project: `${projectConfig.root}/package.json`,
+      entryFile: `${projectConfig.sourceRoot}/index.ts`,
+      external: ['tslib'],
+      rollupConfig: './tools/src/plugins/web-rollup-plugin',
+      assets: [
+        {
+          glob: `${projectConfig.root}/README.md`,
+          input: '.',
+          output: '.',
+        },
+      ],
+    },
+  };
 
   updateProjectConfiguration(tree, schema.project, projectConfig);
 }
