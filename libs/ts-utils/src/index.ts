6d543750db57669f02481480454c0d3b173d44fe
Damian Bielecki <bielik20@gmail.com>
2021-07-06 07:38:57 +0200

    feat(ts-utils): add tapTeardown RxJs operator

--- libs/ts-utils/src/index.ts
+++ libs/ts-utils/src/index.ts
@@ -1,6 +1,7 @@
 export * from './lib/debug/extract-invocation-location';
 export * from './lib/rxjs/omit-undefined';
 export * from './lib/rxjs/tap-once';
+export * from './lib/rxjs/tap-teardown';
 export * from './lib/utils/factory-or-value';
 export * from './lib/utils/falsy';
 export * from './lib/utils/is-not-undefined';
