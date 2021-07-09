a2fe1b95f41a0811df1b431048c27618620b8961
Damian Bielecki <bielik20@gmail.com>
2021-07-09 17:14:29 +0200

    feat(rx-devtools): create RxDevtools decorator

---
+++ libs/rx-devtools/src/lib/get-connection.ts
@@ -0,0 +1,16 @@
+import { RxDevtoolsConnection } from './rx-devtools-connection';
+
+let connection: RxDevtoolsConnection | undefined;
+
+export function getConnection(): RxDevtoolsConnection | undefined {
+  if (typeof window === 'undefined') {
+    return;
+  }
+
+  if (!connection) {
+    const reduxDevTools = (window as any)?.__REDUX_DEVTOOLS_EXTENSION__?.connect();
+    connection = new RxDevtoolsConnection(reduxDevTools);
+  }
+
+  return connection;
+}
--- libs/rx-devtools/src/lib/rx-devtools.ts
+++ libs/rx-devtools/src/lib/rx-devtools.ts
@@ -1,3 +1,62 @@
-export function rxDevtools(): string {
-  return 'rx-devtools';
+import { DEBUGGABLE_KEY, isDebuggable } from '@ns3/rx-state';
+import { BehaviorSubject, merge, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { getConnection } from './get-connection';
+import { StateFragment } from './state-fragment';
+
+export function RxDevtools() {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  return function <T extends { new (...args: any[]): {} }>(target: T): T {
+    return class extends target {
+      constructor(...args: any[]) {
+        super(...args);
+
+        const connection = getConnection();
+
+        if (!connection) {
+          return;
+        }
+
+        const fragments = extractStateFragments(this, target.name);
+
+        merge(...fragments).subscribe((fragment) => connection.update(fragment));
+      }
+    };
+  };
+}
+
+function extractStateFragments(
+  record: Record<string, any>,
+  parent: string,
+): Observable<StateFragment>[] {
+  const fragments = [];
+  for (const key of Object.keys(record)) {
+    const state = extractObservableIfDebuggable(record[key]);
+
+    if (state) {
+      fragments.push(createStateFragment(parent, key, state));
+    }
+  }
+
+  return fragments;
+}
+
+function extractObservableIfDebuggable(input: any): Observable<unknown> | undefined {
+  if (isDebuggable(input)) {
+    return input[DEBUGGABLE_KEY]();
+  }
+
+  if (input instanceof BehaviorSubject) {
+    return input;
+  }
+
+  return undefined;
+}
+
+function createStateFragment(
+  parent: string,
+  name: string,
+  source: Observable<unknown>,
+): Observable<StateFragment> {
+  return source.pipe(map((value) => ({ parent, name, value })));
 }
