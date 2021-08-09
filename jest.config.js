83b57a98d51226e7acedb43da141ef7d4ac742b4
Damian Bielecki <bielik20@gmail.com>
2021-08-09 17:34:33 +0200

    chore: run migrations

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,3 @@
-module.exports = {
-  projects: [
-    '<rootDir>/libs/ts-utils',
-    '<rootDir>/libs/http-client',
-    '<rootDir>/libs/react-di',
-    '<rootDir>/apps/react-demo',
-    '<rootDir>/libs/rx-state',
-    '<rootDir>/libs/react-utils',
-    '<rootDir>/libs/rx-actions',
-    '<rootDir>/libs/rx-devtools',
-  ],
-};
+const { getJestProjects } = require('@nrwl/jest');
+
+module.exports = { projects: getJestProjects() };
