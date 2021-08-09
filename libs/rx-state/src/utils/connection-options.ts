8b56667bdd5a994b208336ed880a1403074d9792
Damian Bielecki <bielik20@gmail.com>
2021-08-09 16:59:27 +0200

    fix(rx-state): rename connecting$ to connecting$$
    BREAKING CHANGE: rename connecting$ to connecting$$

--- libs/rx-state/src/utils/connection-options.ts
+++ libs/rx-state/src/utils/connection-options.ts
@@ -4,16 +4,16 @@ import { ConnectionManagerConfig } from './connection-manager';
 export type ConnectionOptions = Pick<ConnectionManagerConfig<unknown>, 'timeout' | 'strategy'>;
 
 export type ConnectionHooks =
-  | { connecting$: BehaviorSubject<boolean> }
+  | { connecting$$: BehaviorSubject<boolean> }
   | Pick<ConnectionManagerConfig<unknown>, 'connecting' | 'connected'>;
 
 export function normalizeConnectionHooks(
   input: ConnectionHooks,
 ): Pick<ConnectionManagerConfig<unknown>, 'connecting' | 'connected'> {
-  if ('connecting$' in input) {
+  if ('connecting$$' in input) {
     return {
-      connecting: () => !input.connecting$.value && input.connecting$.next(true),
-      connected: () => input.connecting$.value && input.connecting$.next(false),
+      connecting: () => !input.connecting$$.value && input.connecting$$.next(true),
+      connected: () => input.connecting$$.value && input.connecting$$.next(false),
     };
   }
   return input;
