50a7ae2951f3f7f3528cd8611a483e6b6ea1a8e9
Damian Bielecki <bielik20@gmail.com>
2021-10-02 13:48:48 +0200

    feat(react-utils): export result types

--- libs/react-utils/src/index.ts
+++ libs/react-utils/src/index.ts
@@ -3,3 +3,4 @@ export * from './lib/hooks/use-promise';
 export * from './lib/hooks/use-stream';
 export * from './lib/hooks/use-stream-value';
 export * from './lib/hooks/use-unsubscribe';
+export * from './lib/utils/results';
