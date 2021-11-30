5dad8dd2c52e233704acbeeee92a3217d93c4370
Damian Bielecki <bielik20@gmail.com>
2021-11-30 18:23:54 +0100

    chore(rx-state): mark debuggable as not stable
    BREAKING CHANGE: debuggable utils of rx-state are marked as not stable

--- libs/rx-state/src/models/debuggable.ts
+++ libs/rx-state/src/models/debuggable.ts
@@ -1,11 +1,20 @@
 import { Observable } from 'rxjs';
 
+/**
+ * @deprecated NOT STABLE API, SUBJECT TO CHANGE WITHOUT MAJOR RELEASE
+ */
 export const DEBUGGABLE_KEY = '__@ns3/rx-state_debuggable__';
 
+/**
+ * @deprecated NOT STABLE API, SUBJECT TO CHANGE WITHOUT MAJOR RELEASE
+ */
 export interface Debuggable<T = unknown> {
   [DEBUGGABLE_KEY]: () => Observable<T>;
 }
 
+/**
+ * @deprecated NOT STABLE API, SUBJECT TO CHANGE WITHOUT MAJOR RELEASE
+ */
 export function isDebuggable(input: any): input is Debuggable {
   return typeof input[DEBUGGABLE_KEY] === 'function';
 }
