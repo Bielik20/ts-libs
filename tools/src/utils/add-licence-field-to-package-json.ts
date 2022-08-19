fe1c3ae5665939e9c62bcda4804690c5564e6ce3
Damian Bielecki <bielik20@gmail.com>
2022-08-19 09:12:12 +0200

    chore: fix web-lib generator

--- tools/src/utils/add-licence-field-to-package-json.ts
+++ tools/src/utils/add-licence-field-to-package-json.ts
@@ -1,9 +1,9 @@
-import { readProjectConfiguration, Tree, updateJson } from '@nrwl/devkit';
+import { joinPathFragments, readProjectConfiguration, Tree, updateJson } from '@nrwl/devkit';
 
 export function addLicenceFieldToPackageJson(tree: Tree, schema: { project: string }) {
   const projectConfig = readProjectConfiguration(tree, schema.project);
 
-  updateJson(tree, projectConfig.targets.build.options.packageJson, (json) => {
+  updateJson(tree, joinPathFragments(projectConfig.root, 'package.json'), (json) => {
     json.license = 'MIT';
 
     return json;
