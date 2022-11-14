61c6c25ae3682d62b04c8c6a90d10ce91614d821
Damian Bielecki <bielik20@gmail.com>
2022-11-14 19:38:19 +0100

    chore: fix next compilation

--- apps/react-demo/src/layout/app-layout.tsx
+++ apps/react-demo/src/layout/app-layout.tsx
@@ -4,6 +4,7 @@ import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import NextLink from 'next/link';
 import React from 'react';
 import { examples } from 'react-demo/examples/examples';
 import { Link } from 'react-demo/layout/link';
@@ -26,8 +27,8 @@ export const AppLayout: React.FC<Props> = ({ children }) => {
     <>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" className={classes.title} component={Link} naked href="/">
-            Reactive State
+          <Typography variant="h6" className={classes.title}>
+            <NextLink href="/">Reactive State</NextLink>
           </Typography>
           {examples.map((example) => (
             <Button key={example.title} color="inherit" component={Link} naked href={example.url}>
--- apps/react-demo/src/layout/link.tsx
+++ apps/react-demo/src/layout/link.tsx
@@ -1,11 +1,7 @@
-import MuiLink from '@material-ui/core/Link';
 import { LinkTypeMap } from '@material-ui/core/Link/Link';
 import { OverrideProps } from '@material-ui/core/OverridableComponent';
-import clsx from 'clsx';
 import NextLink, { LinkProps as NextLinkProps } from 'next/link';
-import { useRouter } from 'next/router';
 import React from 'react';
-import { UrlObject } from 'url';
 
 type AProps = React.DetailedHTMLProps<
   React.AnchorHTMLAttributes<HTMLAnchorElement>,
@@ -15,43 +11,6 @@ type NextComposedProps = NextLinkProps & AProps;
 type MuiLinkProps = OverrideProps<LinkTypeMap, React.ElementType>;
 export type LinkProps = NextComposedProps & MuiLinkProps;
 
-const NextComposed = React.forwardRef(function NextComposed(props: NextComposedProps, ref) {
-  const { as, href, ...other } = props;
-
-  return (
-    <NextLink href={href} as={as}>
-      <a ref={ref as any} {...other} />
-    </NextLink>
-  );
-});
-
-// A styled version of the Next.js Link component:
-// https://nextjs.org/docs/#with-link
-function StyledLink(props: LinkProps) {
-  const {
-    href,
-    activeClassName = 'active',
-    className: classNameProps,
-    innerRef,
-    naked,
-    ...other
-  } = props;
-
-  const router = useRouter();
-  const pathname = typeof href === 'string' ? href : (href as UrlObject).pathname;
-  const className = clsx(classNameProps, {
-    [activeClassName]: router.pathname === pathname && activeClassName,
-  });
-
-  if (naked) {
-    return <NextComposed className={className} ref={innerRef} href={href} {...other} />;
-  }
-
-  return (
-    <MuiLink component={NextComposed} className={className} ref={innerRef} href={href} {...other} />
-  );
-}
-
 export const Link = React.forwardRef(function Link(props: LinkProps, ref) {
-  return <StyledLink {...props} innerRef={ref} />;
+  return <NextLink ref={ref as any} {...props} />;
 });
