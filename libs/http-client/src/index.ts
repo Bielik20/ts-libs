2435e6133fd30d80ce86e7917ca5242b7d212270
Damian Bielecki <bielik20@gmail.com>
2022-12-15 17:14:10 +0100

    chore: remove old http-client
    BREAKING CHANGE: remove old http-client, use fetch-client instead

--- libs/http-client/src/index.ts
+++ libs/http-client/src/index.ts
@@ -1,7 +1,2 @@
 export * from './lib/fetch-client';
 export * from './lib/fetch-interceptor';
-export * from './lib/http-client';
-export * from './lib/http-interceptors';
-export * from './lib/http-operators';
-export * from './lib/models/http-handler';
-export * from './lib/models/http-interceptor';
--- libs/http-client/src/lib/models/klass.ts
+++
@@ -1,3 +0,0 @@
-export interface Klass<T> extends Function {
-  new (...args: any[]): T;
-}
